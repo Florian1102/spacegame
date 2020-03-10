e3f2dbf94b91a95301489fbcbb541b17c5692376
Florian Dittrich <florian_dittrich@hotmail.de>
2020-03-10 15:01:26 +0100

    fixed bug

--- frontend/src/app/shared/components/timer/timer.component.ts
+++ frontend/src/app/shared/components/timer/timer.component.ts
@@ -19,25 +19,41 @@ export class TimerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setCurrentDeltaTime();
+    this.convertTime();
 
     setInterval(() => {
       if (this.necessaryTime !== null){
 
-        this.currentTime = new Date().getTime();
-        this.deltaTime = new Date(this.necessaryTime).getTime() - this.currentTime;
-        this.days = Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)) || 0;
-        this.hours = Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) || 0;
-        this.minutes = Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)) || 0;
-        this.seconds = Math.floor((this.deltaTime % (1000 * 60)) / 1000) || 0;
-
+        this.setCurrentDeltaTime();
+        this.convertTime();
         this.isRunning = true;
-        if (this.deltaTime < 0 && this.isRunning) {
+
+        if (this.deltaTime <= 0 && this.isRunning) {
           window.location.reload();
           this.isRunning = false;
         };
       }
-
     }, 1000);
+  }
+
+  convertTime(){
+    if (this.deltaTime <= 0){
+      this.days = 0;
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+    } else {
+      this.days = Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)) || 0;
+      this.hours = Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) || 0;
+      this.minutes = Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)) || 0;
+      this.seconds = Math.floor((this.deltaTime % (1000 * 60)) / 1000) || 0;
+    }
+  }
+
+  setCurrentDeltaTime(){
+    this.currentTime = new Date().getTime();
+    this.deltaTime = new Date(this.necessaryTime).getTime() + 1000 - this.currentTime;
   }
 
 }
